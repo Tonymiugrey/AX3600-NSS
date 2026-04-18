2102c1e036235fff52998c9deb9fd55ba9d088b9
Tonymiugrey <tonymiugrey@qq.com>
2026-04-19 02:37:32 +0800

    1

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# SPDX-License-Identifier: MIT
-# Copyright (C) 2026 VIKINGYFY
 
 #移除luci-app-attendedsysupgrade
 sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
@@ -52,8 +50,10 @@ if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 	#取消nss相关feed
 	echo "CONFIG_FEED_nss_packages=n" >> ./.config
 	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
+	#开启sqm-nss插件
+	echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
+	echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
 	#设置NSS版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
 	echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
 	#无WIFI配置调整Q6大小
 	if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
@@ -61,6 +61,43 @@ if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 		find $DTS_PATH -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\).dtsi/ipq\1-nowifi.dtsi/g' {} +
 		echo "qualcommax set up nowifi successfully!"
 	fi
-	#其他调整
-	echo "CONFIG_PACKAGE_kmod-usb-serial-qualcomm=y" >> ./.config
 fi
+
+#修复ath11k-firmware
+#curl -L https://raw.githubusercontent.com/qosmio/openwrt-ipq/refs/heads/main-nss/package/firmware/ath11k-firmware/Makefile -o package/firmware/ath11k-firmware/Makefile
+
+# 1. 禁用核心硬件支持
+sed -i 's/CONFIG_USB_SUPPORT=y/# CONFIG_USB_SUPPORT is not set/' .config
+sed -i 's/CONFIG_EMMC_SUPPORT=y/# CONFIG_EMMC_SUPPORT is not set/' .config
+
+# 2. 禁用默认包含的 USB 相关驱动模块 (DWC3/USB3)
+sed -i 's/CONFIG_DEFAULT_automount=y/# CONFIG_DEFAULT_automount is not set/' .config
+sed -i 's/CONFIG_DEFAULT_kmod-usb3=y/# CONFIG_DEFAULT_kmod-usb3 is not set/' .config
+sed -i 's/CONFIG_DEFAULT_kmod-usb-dwc3=y/# CONFIG_DEFAULT_kmod-usb-dwc3 is not set/' .config
+sed -i 's/CONFIG_DEFAULT_kmod-usb-dwc3-qcom=y/# CONFIG_DEFAULT_kmod-usb-dwc3-qcom is not set/' .config
+
+# 3. 禁用分区与磁盘管理工具 (fdisk/gdisk/blkid等)
+sed -i 's/CONFIG_PACKAGE_blkid=y/# CONFIG_PACKAGE_blkid is not set/' .config
+sed -i 's/CONFIG_PACKAGE_fdisk=y/# CONFIG_PACKAGE_fdisk is not set/' .config
+sed -i 's/CONFIG_PACKAGE_parted=y/# CONFIG_PACKAGE_parted is not set/' .config
+sed -i 's/CONFIG_PACKAGE_sfdisk=y/# CONFIG_PACKAGE_sfdisk is not set/' .config
+sed -i 's/CONFIG_PACKAGE_cgdisk=y/# CONFIG_PACKAGE_cgdisk is not set/' .config
+sed -i 's/CONFIG_PACKAGE_gdisk=y/# CONFIG_PACKAGE_gdisk is not set/' .config
+
+# 4. 禁用文件系统工具与挂载服务
+sed -i 's/CONFIG_DEFAULT_e2fsprogs=y/# CONFIG_DEFAULT_e2fsprogs is not set/' .config
+sed -i 's/CONFIG_DEFAULT_f2fs-tools=y/# CONFIG_DEFAULT_f2fs-tools is not set/' .config
+sed -i 's/CONFIG_PACKAGE_block-mount=y/# CONFIG_PACKAGE_block-mount is not set/' .config
+
+# 5. 禁用 USB 核心内核模块 (kmod-usb-*)
+sed -i 's/CONFIG_PACKAGE_kmod-usb-common=y/# CONFIG_PACKAGE_kmod-usb-common is not set/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-usb-core=y/# CONFIG_PACKAGE_kmod-usb-core is not set/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-usb-storage=y/# CONFIG_PACKAGE_kmod-usb-storage is not set/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-usb-storage-extras=y/# CONFIG_PACKAGE_kmod-usb-storage-extras is not set/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-usb-storage-uas=y/# CONFIG_PACKAGE_kmod-usb-storage-uas is not set/' .config
+
+
+mkdir -p package/base-files/files/etc/uci-defaults && \
+curl -L https://github.com/Mike-qian/OpenWRT-CI/raw/refs/heads/main/99-Configure-ZRam -o package/base-files/files/etc/uci-defaults/99-Configure-ZRam && \
+curl -L https://github.com/Mike-qian/OpenWRT-CI/raw/refs/heads/main/99-replace-apk-mirrors -o package/base-files/files/etc/uci-defaults/99-replace-apk-mirrors
+
